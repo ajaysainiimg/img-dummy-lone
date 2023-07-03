63d15e2260ecc51b469228c29b42b145f3c7ecd7
Mohityogi123 <mohityogi.img@gmail.com>
2023-07-03 14:42:42 +0530

    -

--- assets/js/index.js
+++ assets/js/index.js
@@ -44,6 +44,7 @@ window.onscroll = function () {
 // })(jQuery);
 // });
 
+// cursor js
 if ($(".custom-cursor").length) {
   var cursor = document.querySelector(".custom-cursor__cursor");
   var cursorinner = document.querySelector(".custom-cursor__cursor-two");
@@ -81,3 +82,59 @@ if ($(".custom-cursor").length) {
     });
   });
 }
+
+
+// brand slider
+var swiper = new Swiper(".brandslider", {
+  spaceBetween: 10,
+  loop:true,
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 4,
+    },
+    1024: {
+      slidesPerView: 5,
+    },
+  },
+});
+
+// counter js
+var a = 0;
+$(window).scroll(function() {
+
+  var oTop = $('#counter').offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() > oTop) {
+    $('.counter-value').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+          countNum: countTo
+        },
+
+        {
+
+          duration: 2000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
+
+        });
+    });
+    a = 1;
+  }
+
+});
